c7d52fde1197c2d1dd1ff97fabeee6f6ad627ada
lemmonboys99 <Zonglei-Sun@users.noreply.github.com>
2025-12-25 14:50:06 -0800

    confirm save

--- speech-to-text.py
+++ speech-to-text.py
@@ -1,11 +1,34 @@
 from RealtimeSTT import AudioToTextRecorder
+import pyttsx3
+import asyncio
 
-def process_text(text):
-    print(text)
 
-if __name__ == '__main__':
-    print("Wait until it says 'speak now'")
-    recorder = AudioToTextRecorder()
 
+
+async def speak(text):
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 125)
+
+    try:
+        engine.endLoop()
+    except:
+        pass
+    text = text.replace(",", "")
+    engine.say(text)
+    engine.runAndWait()
+    del engine
+
+async def main():
     while True:
-        recorder.text(process_text)
+        print("Wait until it says 'speak now'")
+        recorder = AudioToTextRecorder(language="en") 
+        text = recorder.text()
+        print(text)
+
+        if text.strip():  # Only speak if text detected
+            await speak(text)
+
+        
+if __name__ == '__main__':
+    asyncio.run(main())
+    
