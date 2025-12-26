53949733073d7c0d3461b03664fcd00be71a0a00
lemmonboys99 <Zonglei-Sun@users.noreply.github.com>
2025-12-25 19:18:45 -0800

    testing attempt with VB audio

--- speech-to-text.py
+++ speech-to-text.py
@@ -1,11 +1,22 @@
 from RealtimeSTT import AudioToTextRecorder
 import pyttsx3
 import asyncio
+from pygame import mixer
+import os
+import random
+import string
+import time
 
 
 
+mixer.init(devicename="CABLE Input (VB-Audio Virtual Cable)")
 
-async def speak(text):
+
+def generate_random_string(length=8):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+
+
+def speak_sync(text):
     engine = pyttsx3.init()
     engine.setProperty('rate', 125)
 
@@ -14,21 +25,39 @@ async def speak(text):
     except:
         pass
     text = text.replace(",", "")
-    engine.say(text)
+    name = f"{generate_random_string()}.wav"
+
+    engine.save_to_file(text,name)
+    #engine.say(text)
     engine.runAndWait()
-    del engine
+    engine.stop()
+
+    time.sleep(0.1)
+
+    mixer.music.load(name)
+    mixer.music.play()
+
+    while mixer.music.get_busy():
+        time.sleep(0.1)
+
+    mixer.music.unload()
+    os.remove(name)
+
+async def speak(text: str):
+    await asyncio.to_thread(speak_sync, text)
 
 async def main():
     while True:
         print("Wait until it says 'speak now'")
-        recorder = AudioToTextRecorder(language="en") 
+        recorder = AudioToTextRecorder(language="en")
         text = recorder.text()
         print(text)
+
 
         if text.strip():  # Only speak if text detected
             await speak(text)
 
-        
+
+       
 if __name__ == '__main__':
     asyncio.run(main())
-    
